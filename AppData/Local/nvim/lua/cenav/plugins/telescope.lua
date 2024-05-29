e1c498257a0b971e8830898d54d1be2da88a38cf
cenav <cnavarro1112@gmail.com>
2024-05-28 23:53:49 -0500

    nvim noice setup

--- AppData/Local/nvim/lua/cenav/plugins/telescope.lua
+++ AppData/Local/nvim/lua/cenav/plugins/telescope.lua
@@ -4,8 +4,11 @@ return {
 
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = '[P]royect [F]ind'})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = '[P]royect Git}'})
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    end)
     vim.keymap.set('n', '<leader>pws', function()
       local word = vim.fn.expand("<cword>")
       builtin.grep_string({ search = word })
@@ -13,9 +16,6 @@ return {
     vim.keymap.set('n', '<leader>pWs', function()
       local word = vim.fn.expand("<cWORD>")
       builtin.grep_string({ search = word })
-    end)
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end)
   end
 }
